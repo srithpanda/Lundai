52222c9a0eacdec867327723c8d8fb6fde8459a1
srithpanda <111577096+srithpanda@users.noreply.github.com>
2026-05-14 22:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template, request, jsonify
 
+app = Flask(__name__)
+
+messages = []
+
+@app.route("/")
+def home():
+    return render_template("index.html", messages=messages)
+
+@app.route("/add-message", methods=["POST"])
+def add_message():
+    data = request.get_json()
+
+    message = data.get("message")
+
+    if message:
+        messages.append(message)
+
+    return jsonify({
+        "success": True,
+        "messages": messages
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
